5a0affe02795061257805232312971f12017863e
Colin Knox <colinknox@66405s-iMac.rule.lan>
2024-04-26 13:45:19 -0400

    Adding starting project.

---
+++ simple-math-functions.py
@@ -0,0 +1,16 @@
+print("We're going to make multiple math problems from TWO values you give me!")
+first_value = input("What's your first value? ")
+second_value = input("And the second? ")
+# addition =
+# subtraction = 
+# multiplication =
+# division = 
+
+print(f'''
+Below are the following problems:
+
+      Addition: {first_value} + {second_value} = {int(first_value + second_value)}
+      Subtraction: {first_value} - {second_value} = {int(first_value - second_value)}
+      Multiplication: {first_value} * {second_value} = {int(first_value * second_value)}
+      Division: {first_value} / {second_value} = {int(first_value / second_value)}
+''')
