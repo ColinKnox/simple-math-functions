305ee8306a1e89e481a8d58690b9c3956607cf96
Colin Knox <colinwknox@gmail.com>
2024-04-26 21:26:07 -0400

    Fixed issues.

--- simple-math-functions.py
+++ simple-math-functions.py
@@ -1,16 +1,17 @@
 print("We're going to make multiple math problems from TWO values you give me!")
-first_value = input("What's your first value? ")
-second_value = input("And the second? ")
-# addition =
-# subtraction = 
-# multiplication =
-# division = 
+first_value = int(input("What's your first value? "))
+second_value = int(input("And the second? "))
+addition = first_value + second_value
+subtraction = first_value - second_value
+multiplication = first_value * second_value
+division = first_value / second_value
 
 print(f'''
 Below are the following problems:
 
-      Addition: {first_value} + {second_value} = {int(first_value + second_value)}
-      Subtraction: {first_value} - {second_value} = {int(first_value - second_value)}
-      Multiplication: {first_value} * {second_value} = {int(first_value * second_value)}
-      Division: {first_value} / {second_value} = {int(first_value / second_value)}
-''')
+      Addition: {first_value} + {second_value} = {addition}
+      Subtraction: {first_value} - {second_value} = {subtraction}
+      Multiplication: {first_value} * {second_value} = {multiplication}
+      Division: {first_value} / {second_value} = {division}
+'''
+)
